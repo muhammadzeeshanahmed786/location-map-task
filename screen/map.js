3ebf299d346a04d0ff4992010a1cf40a8bb94f33
shani2103792 <78692718+shani2103792@users.noreply.github.com>
2021-11-24 10:51:32 +0500

    Add files via upload
    map task location  show alert

---
+++ screen/map.js
@@ -0,0 +1,76 @@
+import MapView, { Marker } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet, Dimensions } from "react-native";
+import * as Location from "expo-location";
+
+export default function LocationMap() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+
+  
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      console.log(location.coords.latitude, "latitude");
+      console.log(location, "location");
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            image={{ uri: "custom_pin" }}
+          /> */}
+        </MapView>
+      ) : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
